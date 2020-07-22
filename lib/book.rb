3a3edcd8dc5e091a88d58ce2d3548c9eb16327a9
Travis McKinstry <mckinstrytg@gmail.com>
2020-07-22 12:37:26 -0600

    Create test to check if author has first name, create method for first name

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,10 @@
 class Book
 
-  attr_reader :info
+  attr_reader :info, :author_first_name
 
   def initialize(info)
     @info = {}
-
+    @author_first_name = info[:author_first_name]
   end
 
 end
--- test/book_test.rb
+++ test/book_test.rb
@@ -5,8 +5,14 @@ require "./lib/book"
 class Booktest < MiniTest::Test
 
   def test_it_exists
-    book1 = Book.new("info")
+    book1 = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
 
     assert_instance_of Book, book1
+  end
+
+  def test_it_has_an_author_first_name
+    book1 = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+    assert_equal "Harper", book1.author_first_name
   end
 end
