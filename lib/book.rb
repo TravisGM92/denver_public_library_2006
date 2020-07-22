816f455714720f493aad4ec0d0ff8f8a0df656b2
Travis McKinstry <mckinstrytg@gmail.com>
2020-07-22 12:43:18 -0600

    Create method to display publication year

--- lib/book.rb
+++ lib/book.rb
@@ -2,7 +2,8 @@ class Book
 
   attr_reader :info, :author_first_name,
               :author_last_name,
-              :title, :publication_date
+              :title, :publication_date,
+              :publication_year
 
   def initialize(info)
     @info = {}
@@ -10,6 +11,7 @@ class Book
     @author_last_name = info[:author_last_name]
     @title = info[:title]
     @publication_date = info[:publication_date]
+    @publication_year = info[:publication_date][-4..-1]
   end
 
 end
