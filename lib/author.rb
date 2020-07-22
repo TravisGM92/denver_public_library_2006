b1a705a8afa9631881e636d7f40153ca5a04c619
Travis McKinstry <mckinstrytg@gmail.com>
2020-07-22 12:51:21 -0600

    Create method to display empty array of books

--- lib/author.rb
+++ lib/author.rb
@@ -1,10 +1,11 @@
 class Author
 
-  attr_reader :info, :name
+  attr_reader :info, :name, :books
 
   def initialize(info)
     @info = {}
     @name = "#{info[:first_name]} " + "#{info[:last_name]}"
+    @books = []
   end
 
 end
