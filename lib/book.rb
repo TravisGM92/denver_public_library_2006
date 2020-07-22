3a44ea29c1cb2d7dd75152f14ebf618cfdfd568e
Travis McKinstry <mckinstrytg@gmail.com>
2020-07-22 12:41:50 -0600

    Create test and method for publication date

--- lib/book.rb
+++ lib/book.rb
@@ -2,13 +2,14 @@ class Book
 
   attr_reader :info, :author_first_name,
               :author_last_name,
-              :title
+              :title, :publication_date
 
   def initialize(info)
     @info = {}
     @author_first_name = info[:author_first_name]
     @author_last_name = info[:author_last_name]
     @title = info[:title]
+    @publication_date = info[:publication_date]
   end
 
 end
--- test/book_test.rb
+++ test/book_test.rb
@@ -27,4 +27,10 @@ class Booktest < MiniTest::Test
 
     assert_equal "To Kill a Mockingbird", book1.title
   end
+
+  def test_it_has_a_publication
+    book1 = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+    assert_equal "July 11, 1960", book1.publication_date
+  end
 end
